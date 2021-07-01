74a00fee5192fe2b975b1aabb0dce5e96a646a8e
redemple332 <marceloredemple332@gmail.com>
2021-07-01 21:53:36 +0800

    Update cart-content.blade.php

--- resources/views/livewire/cart/cart-content.blade.php
+++ resources/views/livewire/cart/cart-content.blade.php
@@ -15,14 +15,23 @@
                 <tr>
                     <td class="thumb"> <img src="{{asset('storage/product_images/'.$cart->options->image)}}" alt=""></td>
                     <td class="details">
-                        <a href="#">{{ $cart->name }}</a>
+                        <a href="{{ route('products.single-item', ['product' => $cart->model->slug]) }}">{{ $cart->name }}</a>
                         <ul>
                             <li><span>Size: {{ $cart->options->size }}</span></li>
                             <li><span>Color: {{ $cart->options->color }}</span></li>
                         </ul>
                     </td>
                     <td class="price text-center"><strong>&#8369;{{ $cart->price }}</strong><br><del class="font-weak"><small>$40.00</small></del></td>
-                    <td class="qty text-center"><input  class="input" type="number" value="{{ $cart->qty }}"></td>
+                    <td class="qty text-center">
+                        <button  wire:click="decreaseQty('{{ $cart->rowId }}')" type="button">-</button>
+                            <input style="text-align:center" class="input" type="number" disabled value="{{ $cart->qty }}">
+                        <button wire:click="increaseQty('{{ $cart->rowId }}')" type="button">+</button>
+                        @if (session()->has('message'))
+                        <div class="alert alert-success">
+                            {{ session('message') }}
+                        </div>
+                        @endif
+                    </td>
                     <td class="total text-center"><strong class="primary-color">&#8369;{{ $cart->subtotal }}</strong></td>
                     <td class="text-right">
                         <button  wire:click="removeCart('{{ $cart->rowId }}')" type="button" class="main-btn icon-btn"><i class="fa fa-close"></i></button>
@@ -33,22 +42,5 @@
                 <h1>Walang laman</h1>
             @endforelse
         </tbody>
-        <tfoot>
-            <tr>
-                <th class="empty" colspan="3"></th>
-                <th>SUBTOTAL</th>
-                <th colspan="2" class="sub-total">&#8369;{{Cart::subtotal()}}</th>
-            </tr>
-            <tr>
-                <th class="empty" colspan="3"></th>
-                <th>SHIPING</th>
-                <td colspan="2">Free Shipping</td>
-            </tr>
-            <tr>
-                <th class="empty" colspan="3"></th>
-                <th>TOTAL</th>
-                <th colspan="2" class="total">$97.50</th>
-            </tr>
-        </tfoot>
     </table>
 </div>
